05284eae9564ff6e002d0165ff351b64a4250795
Nicola Gasparro <nicola.gasp11@gmail.com>
2021-02-06 11:31:18 +0000

    navbar & above the fold

---
+++ main.js
@@ -0,0 +1,38 @@
+// scroll navbar effects 
+
+document.addEventListener('scroll', () => {
+  let nav = document.querySelector('#navbar')
+  let logo = document.querySelector('#logo')
+  if (window.pageYOffset > 100) {
+    nav.classList.add('shadow')
+    nav.classList.add('bg-white')
+    logo.classList.remove('fa-3x')
+    logo.classList.add('fa-2x')
+    nav.style.transition = "0.3s ease"
+    logo.style.transition = "0.5s ease"
+  } else {
+    nav.classList.remove('shadow')
+    nav.classList.remove('bg-white')
+    logo.classList.add('fa-3x')
+  }
+});
+
+
+
+// sidebar mobile
+
+let sidebar = document.querySelector('#sidebar');
+let mobileMenu = document.querySelector('#mobileMenu')
+sidebar.addEventListener('click', () =>{
+  mobileMenu.classList.add('active-sidebar')
+});
+
+mobileMenu.addEventListener('click', ()=> {
+  mobileMenu.classList.remove('active-sidebar')
+});
+
+
+
+
+ 
+ 
